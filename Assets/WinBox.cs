97cfdf3f17ca693128e8187ca97059ef2d19102b
Supap007 <supap.nant@bumail.net>
2025-05-04 18:56:53 +0700

    Save_8

---
+++ Assets/WinBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinBox : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
+
