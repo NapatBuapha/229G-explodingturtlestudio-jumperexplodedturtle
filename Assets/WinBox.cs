48b2ddb9f07d9f52d1830d8dbb640ae017e9c243
Supap007 <supap.nant@bumail.net>
2025-05-04 19:17:45 +0700

    Save_9

--- Assets/WinBox.cs
+++ Assets/WinBox.cs
@@ -7,7 +7,7 @@ public class WinBox : MonoBehaviour
 {
     public string sceneToLoad;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
